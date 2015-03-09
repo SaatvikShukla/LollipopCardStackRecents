f3497237bca6c555aa4ada3f95587071b14436a5
Saatvik Shukla <cam.saatvik@gmail.com>
2015-03-09 17:01:46 +0530

    Original Commit added apktool and initialized the shell

---
+++ shell.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Saatvik Shukla
+# Mon, March 9 2015
+# 1643hrs IST
+
+# To change the recents layout to something like LP
+
+##	Put your framework-res.apk and SystemUI.apk
+## into the folder Original_files
+## and run the script
+
+checkFiles ()
+{
+	echo "Checking for the files"
+	echo "$PWD/original_files/framework-res.apk"
+	if [ -e "$PWD/original_files/framework-res.apk" ] && [ -e "$PWD/original_files/SystemUI.apk" ] 
+	then
+		echo "Files present. Continuing....."
+	else
+		echo "SystemUI.apk or framework-res.apk not present. Check the files."
+		exit
+	fi	
+}
+
+decompile ()
+{
+	echo "Decompiling The SystemUI.apk"
+	
+}
+
+
+checkFiles
